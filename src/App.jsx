2da142cd9cb1a060a679b0608a82a03387ad3f85
Vikar Maulana Arrisyad <vikar.maulana.arrisyad@gmail.com>
2023-08-20 00:43:29 +0700

    component & props

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
-export default function App() {
+import React from "react"
+
+// arrow function
+const Button = (props) => {
+  //distractering js
+  const {children = 'Button', variant = 'bg-black'} = props;
+
   return (
-  <h1 className="text-3xl font-bold underline text-center text-green-500">Hello world!</h1> 
+     <button className={`h-10 px-6 font-semibold rounded-md ${variant} text-white`}>
+          {children}
+    </button>
   )
 }
+
+function App() {
+ return (
+  <div className="flex justify-center bg-blue-600 min-h-screen items-center">
+    <div className="flex gap-x-3">
+      <Button variant="bg-red-600">Login</Button>
+      <Button variant="bg-slate-700">Logout</Button>
+      <Button></Button>
+    </div>
+  </div>
+ ) 
+}
+
+export default App
